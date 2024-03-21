cdd9ca1472f8d7621de0f42f693b7fda2bb74ebc
Khun <khun.ly@bstorm.be>
2024-03-21 08:52:15 +0100

    tets

---
+++ src/app/models/game.model.ts
@@ -0,0 +1,8 @@
+export interface  GameModel {
+  id: number;
+  redPlayerId: number|null;
+  yellowPlayerId: number|null;
+  redPlayerName: string|null;
+  yellowPlayerName: string|null;
+  versusAI: boolean;
+}
