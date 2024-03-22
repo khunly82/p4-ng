e523350e3cf93a0209a96e2add5db1f0d149d832
Khun <khun.ly@bstorm.be>
2024-03-22 08:52:48 +0100

    refactor ui

---
+++ src/app/features/game/components/game-header/game-header.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {GameRowPlayerComponent} from "../game-row-player/game-row-player.component";
+import {GameModel} from "../../models/game.model";
+
+@Component({
+  selector: 'app-game-header',
+  standalone: true,
+    imports: [
+        GameRowPlayerComponent
+    ],
+  templateUrl: './game-header.component.html',
+  styleUrl: './game-header.component.scss'
+})
+export class GameHeaderComponent {
+  @Input()
+  game!: GameModel|null|undefined;
+}
